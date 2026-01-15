59299cea97b0d457b9c5f888012fc761b7bcf37c
edwar javier cuasapud <edwarcuasapud@gmail.com>
2026-01-15 08:46:02 -0500

    se subio la parte inicializer del back en sprint boot

---
+++ backend/agrosurprecios-api/src/main/java/com/agrosurprecios/agrosurprecios_api/AgrosurpreciosApiApplication.java
@@ -0,0 +1,13 @@
+package com.agrosurprecios.agrosurprecios_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosurpreciosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosurpreciosApiApplication.class, args);
+	}
+
+}
---
+++ backend/agrosurprecios-api/src/test/java/com/agrosurprecios/agrosurprecios_api/AgrosurpreciosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrosurprecios.agrosurprecios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosurpreciosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
